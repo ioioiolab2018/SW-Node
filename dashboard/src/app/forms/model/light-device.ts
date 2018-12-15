d081af2aa107e00636e14bf07afe2fbd99f1e4d2
pawelbubak <pawelbubak97@gmail.com>
2018-12-15 13:37:13 +0100

    angular test

---
+++ dashboard/src/app/forms/model/light-device.ts
@@ -0,0 +1,8 @@
+export class LightDevice {
+    constructor(
+        public id: number,
+        public name: string,
+        public pin: number,
+        public lux: number
+    ) {}
+}
--- dashboard/src/app/shared/components/light-devices/light-devices.component.ts
+++ dashboard/src/app/shared/components/light-devices/light-devices.component.ts
@@ -1,14 +1,18 @@
-import { Component, Input, OnInit } from '@angular/core';
-import { LightSettings } from '../../models/light-settings';
+import { Component, Input, OnInit } from "@angular/core";
+import { LightSettings } from "../../models/light-settings";
+import { HttpService } from "src/app/forms/services/http.service";
+import { FormArray, FormGroup, FormControl } from "@angular/forms";
+import { LightDevice } from "src/app/forms/model/light-device";
 
 @Component({
-    selector: 'app-light-devices',
-    templateUrl: './light-devices.component.html',
-    styleUrls: ['./light-devices.component.scss']
+    selector: "app-light-devices",
+    templateUrl: "./light-devices.component.html",
+    styleUrls: ["./light-devices.component.scss"]
 })
 export class LightDevicesComponent implements OnInit {
     _lightDevicesList: LightSettings[];
     isEdited: boolean[];
+    form: FormArray;
 
     @Input()
     set lightDevicesList(val: LightSettings[]) {
@@ -26,10 +30,31 @@ export class LightDevicesComponent implements OnInit {
         return this._lightDevicesList;
     }
 
-    constructor() {}
+    constructor(private httpService: HttpService) {}
 
-    ngOnInit() {}
+    // =======================  INIT  ======================= //
+    ngOnInit() {
+        this.initForm();
+    }
 
+    private initForm(): void {
+        this.form = new FormArray([]);
+    }
+
+    getLightsSetteings(): void {
+        this.httpService.getLightsSettings().subscribe((val: LightDevice[]) => {
+            val.forEach((element: LightDevice) => {
+                this.form.push(new FormGroup({
+                    id: new FormControl(element.id),
+                    name: new FormControl(element.name),
+                    pin: new FormControl(element.pin),
+                    lux: new FormControl(element.lux),
+                }))
+            })
+        })
+    }
+
+    // =======================  EDIT  ======================= //
     cancelDeviceSettings(val: number): void {
         this.isEdited[val] = false;
     }
